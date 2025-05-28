1fde87b6dd4bd3b0c3080f00909816929ab19cc8
Shish <shishfruitwala29@gmail.com>
2025-05-29 01:24:35 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/CRUD-To-do",
+  base: "/CRUD-To-do/",
 });
